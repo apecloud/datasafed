0139dc3164614217838f52f0b9931d546ffcb2cc
x.zhou <x.zhou@apecloud.com>
2023-09-11 17:36:51 +0800

    chore: add comments to the Storage interface

--- pkg/storage/types.go
+++ pkg/storage/types.go
@@ -28,12 +28,33 @@ type StatResult struct {
 	Files     int64
 }
 
+// Storage is the interface that wraps the basic operations of a storage.
+// The Storage interface can also be used to implement common features
+// such as data encryption and compression. Multiple storage implementations
+// can be linked together as a stack to form a composite storage.
 type Storage interface {
+	// Push pushes the contents of the reader to the given path.
+	// If the path exists, it will be overwritten.
 	Push(ctx context.Context, r io.Reader, rpath string) error
+
+	// Pull pulls the contents of the given path to the writer.
 	Pull(ctx context.Context, rpath string, w io.Writer) error
+
+	// Remove removes the file in the given path if recursive is false.
+	// Otherwise the `rpath` parameter can be a directory and all the
+	// contents inside will be removed.
 	Remove(ctx context.Context, rpath string, recursive bool) error
+
+	// Rmdir removes the directory in the given path only if the directory is empty.
 	Rmdir(ctx context.Context, rpath string) error
+
+	// List lists the contents of the given path.
+	// The `rpath` parameter can also be a file, in this case the function
+	// will return a list with a single entry.
 	List(ctx context.Context, rpath string, opt *ListOptions) ([]DirEntry, error)
+
+	// Stat returns the information about the given path.
+	// The `rpath` parameter can be a file.
 	Stat(ctx context.Context, rpath string) (StatResult, error)
 }
 
